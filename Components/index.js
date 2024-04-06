4f761548d88b92833d41d8f6240f3bcd9ad9b6af
Muhangi2 <eliodamutiba@gmail.com>
2024-04-07 00:31:30 +0300

    componentstructure

---
+++ Components/index.js
@@ -0,0 +1,42 @@
+//mainpage
+import About from "./About";
+import Brand from "./Brand";
+import Faq from "./Faq";
+import Feature from "./Feature";
+import Footer from "./Footer";
+import Header from "./Header";
+import HeroSection from "./HeroSection";
+import Information from "./information";
+import Staking from "./Staking";
+import Swap from "./Swap";
+import Welcome from "./Welcome";
+
+//profile
+import ERC20 from "./ERC20";
+import Profile from "./Profile";
+import ProfileMain from "./ProfileMain";
+import SideBar from "./SideBar";
+import Table from "./Table";
+import TableTwo from "./TableTwo";
+import UserProfile from "./UserProfile";
+
+export {
+  About,
+  Brand,
+  Faq,
+  Feature,
+  Footer,
+  Header,
+  HeroSection,
+  Information,
+  Staking,
+  Swap,
+  Welcome,
+  ERC20,
+  Profile,
+  ProfileMain,
+  SideBar,
+  Table,
+  TableTwo,
+  UserProfile,
+};

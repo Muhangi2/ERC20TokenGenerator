fcbc3f56f13fb0cfa0f4a08806f3f242b477bc8b
Muhangi2 <eliodamutiba@gmail.com>
2024-04-09 00:31:51 +0300

    header

--- pages/create.js
+++ pages/create.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React,{useState} from "react";
+import { ERC20,Header,Footer,UserProfile,Transfer,Profile } from "../Components/index";
+import { Contextprovider } from "../Context/index";
+
+
 
 const create = () => {
+  const[active,setactive]=useState(false);
+  const[transfer,setTransfer]=useState(false)
   return <div>create</div>;
 };
 

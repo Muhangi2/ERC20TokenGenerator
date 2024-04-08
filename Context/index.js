4c9567c9275b653e0d49057e0528f827f0c30f17
Muhangi2 <eliodamutiba@gmail.com>
2024-04-08 10:36:15 +0300

    extractloopUPcontrat

--- Context/index.js
+++ Context/index.js
@@ -18,7 +18,7 @@ export const Contextprovider = ({ children }) => {
   const [getUserERC20Listed, setGetUserERC20Listed] = useState([]);
   const [getAllDonation, setGetAllDonation] = useState([]);
   const [fee, setFee] = useState(); //tinakakyenga
-  const [balance, setBalance] = useState("");
+  const [balance, setBalance] = useState();
   const [mainBalance, setMainBalance] = useState();
   const [nativeToken, setNativeToken] = useState();
 
@@ -33,7 +33,39 @@ export const Contextprovider = ({ children }) => {
       setBalance(ethers.utils.formatEther(balance.toString()));
       //set nativetoken
       const nativeToken = await connectingTokenContract();
-      
+      if (account) {
+        const nativeBalance = await nativeToken.balanceOf(account);
+        const nativeName = await nativeToken.name();
+        const nativeSymbol = await nativeToken.symbol();
+        const nativeDecimals = await nativeToken.decimals();
+        const nativeTotalSupply = await nativeToken.totalSupply();
+        const nativeTokenAdress = await nativeToken.address;
+        const nativeToken = {
+          balance: ethers.utils.formatEther(nativeBalance.toString(), "ethers"),
+          name: nativeName,
+          symbol: nativeSymbol,
+          decimals: nativeDecimals,
+          totalSupply: ethers.utils.formatEther(
+            nativeTotalSupply.toString(),
+            "ethers"
+          ),
+          address: nativeTokenAdress,
+        };
+        setNativeToken(nativeToken);
+        console.log(nativeToken, "nativetokennn");
+      }
+
+      //get contract
+      const contract = await connectingToLookUpContract();
+      //get contrat balance
+      if (account == "0x1633B8595ed0847993801600C68e635FB32724D7") {
+        const mainBalance = await contract.getMainBalance();
+        const balanceinethers = ethers.utils.formatEther(
+          mainBalance.toString()
+        );
+        console.log(balanceinethers);
+        setMainBalance(balanceinethers);
+      }
     } catch (error) {}
   };
 };

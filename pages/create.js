c0b896ac23ce379c80347163b74870bee322b654
Muhangi2 <eliodamutiba@gmail.com>
2024-04-09 21:33:23 +0300

    ui

--- pages/create.js
+++ pages/create.js
@@ -1,12 +1,19 @@
-import React,{useState} from "react";
-import { ERC20,Header,Footer,UserProfile,Transfer,Profile } from "../Components/index";
+import { useState, useContext, useEffect } from "react";
+import {
+  ERC20,
+  Header,
+  Footer,
+  UserProfile,
+  Transfer,
+  Profile,
+} from "../Components/index";
+
 import { Contextprovider } from "../Context/index";
 
-
-
 const create = () => {
-  const[active,setactive]=useState(false);
-  const[transfer,setTransfer]=useState(false)
+  const [active, setactive] = useState(false);
+  const [transfer, setTransfer] = useState(false);
+  const {} = useContext();
   return <div>create</div>;
 };
 

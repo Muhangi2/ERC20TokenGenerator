b72e0777532dc938f16cb0ee98edf0da733c36ea
Muhangi2 <eliodamutiba@gmail.com>
2024-04-10 09:40:03 +0300

    homepage

--- pages/create.js
+++ pages/create.js
@@ -13,8 +13,62 @@ import { Contextprovider } from "../Context/index";
 const create = () => {
   const [active, setactive] = useState(false);
   const [transfer, setTransfer] = useState(false);
-  const {} = useContext();
-  return <div>create</div>;
+  const {
+    address,
+    setAddress,
+    getAllERC20Listed,
+    setGetAllERC20Listed,
+    getUserERC20Listed,
+    setGetUserERC20Listed,
+    getAllDonation,
+    setGetAllDonation,
+    withdrawfunds,
+    fee,
+    setFee,
+    balance,
+    setBalance,
+    mainBalance,
+    setMainBalance,
+    nativeToken,
+    setNativeToken,
+    fetchInitialData,
+    createERC20,
+    transferNativeToken,
+    donateFunds,
+  } = Contextprovider();
+
+  return (
+    <div>
+      <Header />
+      {active && <ERC20 setActive={setactive} createERC20={createERC20} />}
+      {transfer && (
+        <Transfer
+          setTransfer={setTransfer}
+          transferNativeToken={transferNativeToken}
+        />
+      )}
+      <main>
+        <UserProfile />
+        <Profile
+          nativeToken={nativeToken}
+          transferNativeToken={transferNativeToken}
+          mainBalance={mainBalance}
+          balance={balance}
+          getAllDonation={getAllDonation}
+          withdrawfunds={withdrawfunds}
+          getAllERC20Listed={getAllERC20Listed}
+          getUserERC20Listed={getUserERC20Listed}
+          createERC20={createERC20}
+          donateFunds={donateFunds}
+          setActive={setactive}
+          setTransfer={setTransfer}
+          address={address}
+          fee={fee}
+        />
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default create;

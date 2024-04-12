60f75336c4a94ba211813db59a78fc45b88f46e7
Muhangi2 <eliodamutiba@gmail.com>
2024-04-13 01:17:25 +0300

    footerlayout

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,7 +1,54 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const coinList =[
+    "Ripple coin",
+    "Bitcoin",
+    "Ethereum",
+    "Litecoin",
+    "coin base",
+    "skrill card"
+  ]
+  const menuList=[
+    {
+      name:"Home",
+      link:"/"
+    },
+    {
+      name:"ERC20",
+      link:"#"
+    },
+    {
+      name:"Contact Us",
+      link:"#"
+
+    },
+    {
+      name:"About Us",
+      link:"#"
+    },
+    {
+      name:"Blog",
+      link:"#",
+    }
+    ,{
+      name:"Details",
+      link:"#"
+    }
+  ]
+  return <footer className="footer1">
+         <div className="footer-area">
+          <div className="container">
+            <div className="row">
+              <div className="col-xl-4 col-lg-4 col-md-4">
+                <div className="footer-content logo-footer">
+     
+                </div>
+              </div>
+            </div>
+          </div>
+         </div>
+  </footer>;
 };
 
 export default Footer;

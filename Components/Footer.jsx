72b73d410e8038e6ef773350e921b6d434b8130a
Muhangi2 <eliodamutiba@gmail.com>
2024-04-13 01:43:27 +0300

    latest

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -40,10 +40,64 @@ const Footer = () => {
          <div className="footer-area">
           <div className="container">
             <div className="row">
+              {/* firstitemrow */}
               <div className="col-xl-4 col-lg-4 col-md-4">
                 <div className="footer-content logo-footer">
-     
+                    <div className="footer-head">
+                      <div className="footer-logo">
+                        <a href="#">
+                          <img src="img/logo/logo2.png" alt=""/>
+                        </a>
+                      </div>
+                      {/* footer-icons */}
+                      <div className="footer-icons">
+                          <ul>
+                            {
+                              [1,2,3,4,5].map((social,i)=>{
+                                <li>
+                                  <a href="#"> 
+                                  <img src={`img/about/midea${i+1}.png`} alt=""/>
+                                  </a>
+                                </li>
+                              }
+
+                              )
+                            }
+                          </ul>
+                      </div>
+                    </div>
                 </div>
+              </div>
+              {/* anotheriteminrow */}
+              <div className="col-xl-4 col-lg-4 col-md-4">
+                <div className="footer-content rs-mar-0">
+                  <div className="footer-head">
+                      <h4>Patnents Option</h4>
+                      <ul className="footer-list">
+                        {
+                          coinList.map((coin,i)=>{
+                            <li>
+                              <a>{coin}</a>
+                            </li>
+                          })
+                        }
+                      </ul>
+                      <ul className="footer-list">
+                        {menuList.map((menu,i)=>{
+                          <li>
+                            <a href={menu.link}>{menu.name}</a>
+                          </li>
+                        })}
+                      </ul>
+                  </div>
+                  </div> 
+
+              </div>
+            </div>
+            {/* the third item in row */}
+            <div className="col-xl-4 col-lg-4 col-md-4">
+              <div className="footer-content last-content rs-mar-0">
+                
               </div>
             </div>
           </div>

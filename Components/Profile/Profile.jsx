9f8e0d59ddfec9af2feb593896c76b335092794b
Muhangi2 <eliodamutiba@gmail.com>
2024-04-14 23:56:33 +0300

    addedusercomponent

--- Components/Profile/Profile.jsx
+++ Components/Profile/Profile.jsx
@@ -21,7 +21,16 @@ setTransfer,
 address,
 fee
 }) => {
-  return <div>Profile</div>;
+  //states
+  const [open,setOpen]=useState("Dashboard");
+  return <div className="dashboard-area bg-color area-padding-3">
+      <div className="container">
+         <div className="row">
+          <SideBar address={address} setOpen={setOpen} open={open} setActive={setActive} setTransfer={setTransfer}/>
+          <ProfileMain nativeToken={nativeToken} mainBalance={mainBalance} getAllDonation={getAllDonation} donateFunds={donateFunds} withdrawfunds={withdrawfunds} balance={balance} createERC20={createERC20} setOpen={setOpen} open={open} fee={fee} address={address} getAllERC20Listed={getAllERC20Listed} getUserERC20Listed={getUserERC20Listed}/>
+         </div>
+      </div>
+  </div>;
 };
 
 export default Profile;
